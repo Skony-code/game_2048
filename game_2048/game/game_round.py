4715a91d29b2b4952b052ada620d2c16bec3929f
Skony-code <Skony-code@users.noreply.github.com>
2019-12-12 20:41:30 +0100

    Added deepcopy to array

--- game_2048/game/game_round.py
+++ game_2048/game/game_round.py
@@ -1,30 +1,88 @@
 from random import randint
-def end(arr = [[0]*4 for i in range(4)]):
-    for i in range(len(arr)-1):
-        for j in range(len(arr[0])-1):
-            if(arr[i][j]==arr[i+1][j] or arr[i][j]==arr[i][j+1]):
+from copy import deepcopy
+from msvcrt import getch
+def end(arr):
+    ar=deepcopy(arr)
+    for i in range(len(ar) - 1):
+        for j in range(len(ar[0]) - 1):
+            if(ar[i][j]==ar[i + 1][j] or ar[i][j]==ar[i][j + 1]):
                 return False
     return True
-def addrandomTile(arr = [[0]*4 for i in range(4)]):
-    x = sum(row.count(0) for row in arr)
+def addrandomTile(arr):
+    ar = deepcopy(arr)
+    x = sum(row.count(0) for row in ar)
     y = randint(0,x)
     z = 0;
-    print(y)
-    for i in range(len(arr)):
-        for j in range(len(arr[0])):
-            if arr[i][j] == 0:
+    for i in range(len(ar)):
+        for j in range(len(ar[0])):
+            if ar[i][j] == 0:
                 z +=1
                 if z==y:
-                    print(arr)
                     if randint(0,1)==1:
-                        arr[i][j]=2
-                        return arr
+                        ar[i][j]=2
+                        return ar
                     else:
-                        arr[i][j]=4
-                        return arr
+                        ar[i][j]=4
+                        return ar
+
+def move(arr, direction=1):
+    ar = deepcopy(arr)
+    if(direction==1):
+        for h in range(3):
+            for i in range(1,4):
+                for j in range(4):
+                    if ar[i - 1][j]==0:
+                        ar[i - 1][j]=ar[i][j]
+                        ar[i][j]=0
+                    elif ar[i - 1][j]==ar[i][j]:
+                        ar[i - 1][j]*=2
+                        ar[i][j]=0
+
+    elif(direction==2):
+        for h in range(3):
+            for i in range(1,4):
+                for j in range(4):
+                    if ar[i - 1][j]==0:
+                        ar[i - 1][j]=ar[i][j]
+                        ar[i][j]=0
+                    elif ar[i - 1][j]==ar[i][j]:
+                        ar[i - 1][j]*=2
+                        ar[i][j]=0
+    elif(direction==3):
+        for h in range(3):
+            for i in range(1,4):
+                for j in range(4):
+                    if ar[i - 1][j]==0:
+                        ar[i - 1][j]=ar[i][j]
+                        ar[i][j]=0
+                    elif ar[i - 1][j]==ar[i][j]:
+                        ar[i - 1][j]*=2
+                        ar[i][j]=0
+    elif(direction==4):
+        for h in range(3):
+            for i in range(1,4):
+                for j in range(4):
+                    if ar[i - 1][j]==0:
+                        ar[i - 1][j]=ar[i][j]
+                        ar[i][j]=0
+                    elif ar[i - 1][j]==ar[i][j]:
+                        ar[i - 1][j]*=2
+                        ar[i][j]=0
+    return ar
+
 def round():
     arr = [[0]*4 for i in range(4)]
-    while not end(arr):
+    ifend = False
+    arr2 = addrandomTile(arr)
+    arr = deepcopy(arr2)
+    while not ifend:
         arr2=addrandomTile(arr)
-        arr=arr2.copy()
+        arr=deepcopy(arr2)
+        ifend = end(arr)
+        print(arr)
+        while(arr2 == arr):
+            arr2=move(arr,1)
+        print(arr)
+        arr = deepcopy(arr2)
+        print(arr)
 
