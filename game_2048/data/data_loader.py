86db44cebef51b3851bfb3f929f27840ea9be491
Skony-code <skony7@gmail.com>
2019-12-16 11:38:28 +0100

    Added draw and move functions to Tile class

--- game_2048/data/data_loader.py
+++ game_2048/data/data_loader.py
@@ -1,6 +1,6 @@
 import configparser
 
 config = configparser.ConfigParser()
-config.read('D:\Pythonpro\game_2048\game_2048\data\settings.ini')
+config.read("C:\\Users\\mateu\\PycharmProjects\\game_2048\\game_2048\\data\\settings.ini")
 width = config.getint('Window', 'width')
 height = config.getint('Window', 'height')
--- game_2048/graphics/Tile.py
+++ game_2048/graphics/Tile.py
@@ -1,5 +1,25 @@
+from game_2048.lib.graphics import *
+
+
 class Tile:
-    def __init__(self,x,y,number):
-        self.x=x
-        self.y=y
-        self.number=number
+    def __init__(self, x, y, number, side):
+        self.x = x
+        self.y = y
+        self.number = number
+        self.side = side
+        self.rect = Rectangle(Point(x - side / 2, y - side / 2), Point(x + side / 2, y + side / 2))
+        self.text = Text(Point(x, y), str(number))
+
+    def draw(self, win):
+        self.rect.draw(win)
+        self.text.draw(win)
+
+    def move(self, dir, step):
+        if dir == 1:
+            self.y -= self.side * step
+        elif dir == 2:
+            self.x += self.side * step
+        elif dir == 3:
+            self.y += self.side * step
+        elif dir == 4:
+            self.x -= self.side * step
