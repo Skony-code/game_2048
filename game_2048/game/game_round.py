ef804998a8d21fc139c8d1fcdcb8d49ebd775c5b
Skony-code <skony7@gmail.com>
2019-12-19 18:24:18 +0100

    Added prototype slide animation

--- game_2048/game/game_round.py
+++ game_2048/game/game_round.py
@@ -23,13 +23,13 @@ def end(ar):
 def addrandomTile(ar):
     x = sum(row.count(0) for row in ar)
     y = randint(0, x - 1)
-    z = -1;
+    z = -1
     for i in range(len(ar)):
         for j in range(len(ar[0])):
             if ar[i][j] == 0:
                 z += 1
                 if z == y:
-                    if randint(0, 1) == 1:
+                    if randint(0, 2) != 1:
                         ar[i][j] = 2
                         return ar
                     else:
@@ -37,54 +37,70 @@ def addrandomTile(ar):
                         return ar
 
 
-def move(ar, direction=1):
+def move(ar, direction,grid):
     if direction == 1:
         for h in range(3):
             for i in range(1, 4):
                 for j in range(4):
-                    if ar[i - 1][j] == 0:
+                    if ar[i - 1][j] == 0 and ar[i][j]!=0:
                         ar[i - 1][j] = ar[i][j]
                         ar[i][j] = 0
-                    elif ar[i - 1][j] == ar[i][j]:
+                        grid.animateGridTile(i,j,1,direction,1)
+                        grid.updateGrid(ar)
+                    elif ar[i - 1][j] == ar[i][j] and ar[i][j]!=0:
                         ar[i - 1][j] *= 2
                         ar[i][j] = 0
+                        grid.animateGridTile(i, j, 1, direction, 1)
+                        grid.updateGrid(ar)
 
     elif direction == 2:
         for h in range(3):
             for i in range(2, -1, -1):
                 for j in range(4):
-                    if ar[j][i + 1] == 0:
+                    if ar[j][i + 1] == 0 and ar[j][i]!=0:
                         ar[j][i + 1] = ar[j][i]
                         ar[j][i] = 0
-                    elif ar[j][i + 1] == ar[j][i]:
+                        grid.animateGridTile(j, i, 1, direction, 1)
+                        grid.updateGrid(ar)
+                    elif ar[j][i + 1] == ar[j][i] and ar[j][i]!=0:
                         ar[j][i + 1] *= 2
                         ar[j][i] = 0
+                        grid.animateGridTile(j, i, 1, direction, 1)
+                        grid.updateGrid(ar)
     elif direction == 3:
         for h in range(3):
             for i in range(2, -1, -1):
                 for j in range(4):
-                    if ar[i + 1][j] == 0:
+                    if ar[i + 1][j] == 0 and ar[i][j]!=0:
                         ar[i + 1][j] = ar[i][j]
                         ar[i][j] = 0
-                    elif ar[i + 1][j] == ar[i][j]:
+                        grid.animateGridTile(i, j, 1, direction, 1)
+                        grid.updateGrid(ar)
+                    elif ar[i + 1][j] == ar[i][j] and ar[i][j]!=0:
                         ar[i + 1][j] *= 2
                         ar[i][j] = 0
+                        grid.animateGridTile(i, j, 1, direction, 1)
+                        grid.updateGrid(ar)
     elif direction == 4:
         for h in range(3):
             for i in range(1, 4):
                 for j in range(4):
-                    if ar[j][i - 1] == 0:
+                    if ar[j][i - 1] == 0 and ar[j][i]!=0:
                         ar[j][i - 1] = ar[j][i]
                         ar[j][i] = 0
-                    elif ar[j][i - 1] == ar[j][i]:
+                        grid.animateGridTile(j, i, 1, direction, 1)
+                        grid.updateGrid(ar)
+                    elif ar[j][i - 1] == ar[j][i] and ar[j][i]!=0:
                         ar[j][i - 1] *= 2
                         ar[j][i] = 0
+                        grid.animateGridTile(j, i, 1, direction, 1)
+                        grid.updateGrid(ar)
 
 
 def round(win, width, height):
     arr = [[0] * 4 for i in range(4)]
-    grid = Grid(width / 2, height / 2, 400)
-    grid.draw(win)
+    grid = Grid(width / 2, height / 2, 400,win)
+    grid.draw()
     addrandomTile(arr)
     while True:
         addrandomTile(arr)
@@ -98,4 +114,4 @@ def round(win, width, height):
             text.draw(win)
             break
         while arr2 == arr:
-            move(arr, arrowkey())
+            move(arr, arrowkey(),grid)
--- game_2048/graphics/Tile.py
+++ game_2048/graphics/Tile.py
@@ -1,4 +1,5 @@
 from game_2048.lib.graphics import *
+import time
 
 
 def color(number):
@@ -55,13 +56,52 @@ class Tile:
         else:
             self.text.setText("")
         self.rect.setFill(color(number))
+    def move(self,x,y):
+        self.x+=x
+        self.y+=y
+        self.rect.move(x,y)
+        self.text.move(x,y)
+    def moveanimation(self, tiles, dir, tim, win):
+        tile = self.rect.clone()
+        tiletext = self.text.clone()
+        tiletext.setText(str(self.number))
+        self.changeNumber(0)
+        tile.draw(win)
+        tiletext.draw(win)
+        if dir==1:
+            for i in range(tiles * tim * 30):
+                tile.move(0, -self.side / (tim * 30))
+                tiletext.move(0, -self.side / (tim * 30))
+                time.sleep(tim / 30)
+        elif dir == 2:
+            for i in range(tiles * tim * 30):
+                tile.move(self.side / (tim * 30), 0)
+                tiletext.move(self.side / (tim * 30), 0)
+                time.sleep(tim / 30)
+        elif dir == 3:
+            for i in range(tiles * tim * 30):
+                tile.move(0, self.side / (tim * 30))
+                tiletext.move(0, self.side / (tim * 30))
+                time.sleep(tim / 30)
+        elif dir == 4:
+            for i in range(tiles * tim * 30):
+                tile.move(-self.side / (tim * 30), 0)
+                tiletext.move(-self.side / (tim * 30), 0)
+                time.sleep(tim / 30)
+        tile.undraw()
+        tiletext.undraw()
+        del tile
+        del tiletext
+
+
 
 
 class Grid:
-    def __init__(self, x, y, side):
+    def __init__(self, x, y, side,win):
         self.side = side
         self.x = x
         self.y = y
+        self.win=win
         self.bigrect = Rectangle(Point(x+side/2+10,y+side/2+10),Point(x-side/2-10,y-side/2-10))
         self.bigrect.setFill(color_rgb(150,150,150))
         self.tiles = [[] for i in range(4)]
@@ -69,11 +109,11 @@ class Grid:
             for j in range(4):
                 self.tiles[i].append( Tile(x - 1.5 * side/4 + i * side/4, y - 1.5 * side/4 + j * side/4, 0, side / 4 - 5))
 
-    def draw(self, win):
-        self.bigrect.draw(win)
+    def draw(self):
+        self.bigrect.draw(self.win)
         for i in range(4):
             for j in range(4):
-                self.tiles[j][i].draw(win)
+                self.tiles[j][i].draw(self.win)
 
     def updateTile(self, i, j, number):
         self.tiles[i][j].changeNumber(number)
@@ -82,5 +122,17 @@ class Grid:
         for i in range(4):
             for j in range(4):
                 self.tiles[j][i].changeNumber(arr[i][j])
-    def animateGrid(self,ar,arr,key):
-        pass
+    def animateGridTile(self,i,j,tiles,dir,time):
+        self.tiles[j][i].moveanimation(tiles,dir,time,self.win)
+    def animateGrid(self,ar,arr,key):#TODO ADD ANIMATIONS
+        for h in range(3):
+            for i in range(4):
+                for j in range(4):
+                    if key==1:
+                        pass
+                    elif key == 2:
+                        pass
+                    elif key == 3:
+                        pass
+                    elif key == 4:
+                        pass
